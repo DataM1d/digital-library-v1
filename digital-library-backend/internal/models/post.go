1371ef0b6c9a4d248e42d9bb4abba3f597933aa0
Data <d9044839@gmail.com>
2026-04-19 17:48:27 +0200

    fix: Forgot to pass the json tags tot the posts Post struct

--- digital-library-backend/internal/models/post.go
+++ digital-library-backend/internal/models/post.go
@@ -3,23 +3,23 @@ package models
 import "time"
 
 type Post struct {
-	ID              int
-	CreatedBy       int
-	CategoryID      int
-	LastModifiedBy  int
-	LikeCount       int
-	Title           string
-	Content         string
-	ImageURL        string
-	BlurHash        string
-	Slug            string
-	Status          string
-	AltText         *string
-	MetaDescription *string
-	OGImage         *string
-	CreatedAt       time.Time
-	UpdatedAt       time.Time
-	CategoryName    string
-	UserHasLiked    bool
-	Tags            []string
+	ID              int       `json:"id"`
+	CreatedBy       int       `json:"created_by"`
+	CategoryID      int       `json:"category_id"`
+	LastModifiedBy  int       `json:"last_modified_by"`
+	LikeCount       int       `json:"like_count"`
+	Title           string    `json:"title"`
+	Content         string    `json:"content"`
+	ImageURL        string    `json:"image_url"`
+	BlurHash        string    `json:"blur_hash"`
+	Slug            string    `json:"slug"`
+	Status          string    `json:"status"`
+	AltText         *string   `json:"alt_text"`
+	MetaDescription *string   `json:"meta_description"`
+	OGImage         *string   `json:"og_image"`
+	CreatedAt       time.Time `json:"created_at"`
+	UpdatedAt       time.Time `json:"updated_at"`
+	CategoryName    string    `json:"category_name"`
+	UserHasLiked    bool      `json:"user_has_liked"`
+	Tags            []string  `json:"tags"`
 }
